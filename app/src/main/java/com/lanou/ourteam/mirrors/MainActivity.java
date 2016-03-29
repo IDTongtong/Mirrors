77f7765051c19e4d4fea415feb0cc9675d661d92
佟野 <1215353560@qq.com>
2016-03-29 19:22:13 +0800

    假图片的欢迎页完成

--- app/src/main/java/com/lanou/ourteam/mirrors/MainActivity.java
+++ app/src/main/java/com/lanou/ourteam/mirrors/MainActivity.java
@@ -1,13 +1,28 @@
 package com.lanou.ourteam.mirrors;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.lanou.ourteam.mirrors.activity.WelcomeActivity;
+import com.lanou.ourteam.mirrors.base.BaseActivity;
+
+public class MainActivity extends BaseActivity {
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected int setContent() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void initView() {
+      jumpToActivity(this, WelcomeActivity.class, null);
+
     }
 }
