a7205100e4c20c55f8ad65dffdf9a0423ec6cd9a
张桐 <836074604@qq.com>
2016-03-30 10:59:33 +0800

    图片已经改名  创建账户布局完成

---
+++ app/src/main/java/com/lanou/ourteam/mirrors/activity/CreateAccountActivity.java
@@ -0,0 +1,29 @@
+package com.lanou.ourteam.mirrors.activity;
+
+import com.lanou.ourteam.mirrors.R;
+import com.lanou.ourteam.mirrors.base.BaseActivity;
+
+/**
+ * Created by zt on 16/3/30.
+ */
+public class CreateAccountActivity extends BaseActivity{
+
+
+
+
+
+    @Override
+    protected int setContent() {
+        return R.layout.activity_createaccount;
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+}
