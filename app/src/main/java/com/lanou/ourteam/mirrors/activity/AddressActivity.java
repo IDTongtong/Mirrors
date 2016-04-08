a47928582db232bb44257532d7926def1833a17a
张桐 <836074604@qq.com>
2016-04-08 20:57:39 +0800

    no message
    Signed-off-by: 张桐 <836074604@qq.com>

---
+++ app/src/main/java/com/lanou/ourteam/mirrors/activity/AddressActivity.java
@@ -0,0 +1,23 @@
+package com.lanou.ourteam.mirrors.activity;
+
+import com.lanou.ourteam.mirrors.base.BaseActivity;
+
+/**
+ * Created by zt on 16/4/8.
+ */
+public class AddressActivity extends BaseActivity {
+    @Override
+    protected int setContent() {
+        return 0;
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+}
