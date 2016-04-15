b7ac771dc0dd2316e7b1323bd02a6e881dbce512
张桐 <836074604@qq.com>
2016-04-15 15:40:00 +0800

    提交修改

--- app/src/main/java/com/lanou/ourteam/mirrors/base/BaseActivity.java
+++ app/src/main/java/com/lanou/ourteam/mirrors/base/BaseActivity.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.facebook.drawee.backends.pipeline.Fresco;
 
@@ -25,6 +27,9 @@ public abstract class BaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,  WindowManager.LayoutParams.FLAG_FULLSCREEN);
+      //  实现无标题栏（但有系统自带的任务栏）：
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         BaseApplication.addActivity(this);
 
         //Fresco初始化:相关
